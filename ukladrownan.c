39e7ea3712ff3acad39ddc0c358c02ae74d80e9a
kumy74 <tomek.pl@poczta.fm>
2017-11-23 09:12:08 +0100

    Poczatek

---
+++ ukladrownan.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main()
+
+{
+	puts("Układ równań metoda wyznacznikow");
+	float a1,b1,c1,a2,b2,c2;
+	printf("Podaj wartość argumentu a1: "); scanf("%f", &a1);
+	printf("Podaj wartość argumentu b1: "); scanf("%f", &b1);
+	printf("Podaj wartość argumentu c1: "); scanf("%f", &c1);
+        printf("Podaj wartosc argumentu a2: "); scanf("%f", &a2);
+//	delta=(b*b)-(4*a*c);
+//	if(a==0){puts("Aby rowananie bylo kwadratowe 'a' musi być RÓŻNE od zera!");}
+//	else{
+//		if(delta>0){
+//			pdelta=sqrt(delta);
+//			printf("Równanie ma dwa rozwiązania: x1=%.2f x2=%.2f\n", (-b-pdelta)/(2*a),(-b+pdelta)/(2*a));}
+//			else if(delta==0){printf("Równanie na jedno rozwiązanie: x=%.2f\n", -b/(2*a));}
+//			else if(delta<0){printf("Równanie nie ma rozwiązań rzeczywistych!\n");}
+
+
+		}
+return 0;
+
+}
+
+
